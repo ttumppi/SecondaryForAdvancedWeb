d9ad2eece39140c90d43827601b297d6314effa9
tuomas lamminniemi <tuomas@lamminniemi.com>
2025-01-10 00:01:30 +0200

    Added basic html, css and js files.

---
+++ src/singlePage.js
@@ -0,0 +1,15 @@
+const button = document.getElementById("fontButton");
+
+button.onclick = () => {
+
+    const helloWorldText = document.getElementById("helloWorld");
+
+    if (helloWorldText.style.fontSize != "25px"){
+        helloWorldText.style.fontSize = "25px";
+    }
+    else{
+        helloWorldText.style.fontSize = "10px";
+    }
+
+}
+
